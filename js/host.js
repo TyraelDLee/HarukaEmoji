c95503461b24edbcb0c465dbad1e52a0619400e9
TyraelDLee <lee.tyreal12138@outlook.com>
2021-06-26 23:22:29 +1000

    minor improvements

--- js/host.js
+++ js/host.js
@@ -27,7 +27,7 @@ const popup = document.createElement("div");
 const selec = document.createElement("div");
 var emojiTable = document.createElement("table");
 var sideBar = document.getElementById("aside-area-vm");
-var commentsTextArea;
+var commentsTextArea = null;
 // add new emoji text here.
 const emoji = [["(傻豹)","(吃桃)"],
     ["(rua豹)","(问号豹)"],
@@ -56,60 +56,59 @@ popup.style.left = document.body.clientWidth - absLocHor + 'px';
 popup.style.top = "100px";
 selec.style.left = document.body.clientWidth - absLocHor - 160 +"px";
 selec.style.top = "95px";
+var oLoc = [0,0];
+var cLoc = [0,0];
 window.onload=function(){
     popup.onmousedown = function (ev) {
         popup.className = "popup-click-in";
         var oevent = ev || event;
-
         var distanceX = oevent.clientX - popup.offsetLeft;
         var distanceY = oevent.clientY - popup.offsetTop;
-
-        document.onmousemove = function (ev) {
+        oLoc = [popup.offsetLeft, popup.offsetTop];
+        cLoc = [popup.offsetLeft, popup.offsetTop];
+        popup.onmousemove = function (ev) {
             isDrag = 1;
             var oevent = ev || event;
-            popupLocHor = oevent.clientX - distanceX
-            popupLocVac = oevent.clientY - distanceY
+            popupLocHor = oevent.clientX - distanceX;
+            popupLocVac = oevent.clientY - distanceY;
+            cLoc = [popupLocHor, popupLocVac];
             popup.style.left = popupLocHor + 'px';
             popup.style.top = popupLocVac + 'px';
             selec.style.left = popupLocHor - 160 + "px";
             selec.style.top = popupLocVac - 5 + "px";
             absLocHor = document.body.clientWidth - popupLocHor;
         };
-        document.onmouseup = function () {
-            document.onmousemove = null;
-            document.onmouseup = null;
+        popup.onmouseup = function () {
+            popup.className = "popup-click-out";
+            if(isMoved(oLoc[0], oLoc[1], cLoc[0], cLoc[1])){
+                if (selec.style.display === "none") {
+                    selec.style.display = "block";
+                    selec.className = "selection-fade-in";
+                } else {
+                    selec.className = "selection-fade-out";
+                    setTimeout(hide, 150);
+                }
+            }
+            popup.onmousemove = null;
+            popup.onmouseup = null;
         };
-    };
-
-    popup.onmouseup = function (ev){
-        popup.className = "popup-click-out";
     };
 
     popup.onmouseenter = function (){
         popup.className = "popup-click-hoverin";
-    }
+    };
 
     popup.onmouseleave = function (){
         popup.className = "popup-click-hoverout";
-    }
-
-    popup.onclick = function (e){
-        if (isDrag === 0) {
-            if (selec.style.display === "none") {
-                selec.style.display = "block";
-                selec.className = "selection-fade-in";
-            } else {
-                selec.className = "selection-fade-out";
-                setTimeout(hide, 150);
-            }
-        } else {
-            isDrag = 0;
-        }
     };
 }
 
 function hide(){
     selec.style.display = "none";
+}
+
+function isMoved(oX, oY, cX, cY){
+    return Math.abs(oX - cX) === 0 && Math.abs(oY - cY) === 0;
 }
 
 window.addEventListener("resize", function(){
@@ -121,48 +120,55 @@ setTimeout(delay,5000);
 function delay(){
     console.log("load complete")
     var button = sideBar.getElementsByTagName("button")[sideBar.getElementsByTagName("button").length-1];
+    var emojiText = "";
     button.addEventListener("click", buttonHandler);
     function buttonHandler(){
         emojiText = "";
     }
 
     commentsTextArea = sideBar.getElementsByTagName("textarea")[0];
-    commentsTextArea.addEventListener("focus", textareaHandler);
-    commentsTextArea.addEventListener("blur", textareaHandler);
-    function textareaHandler(){
-        commentsTextArea.value = emojiText;
-    }
 
-    commentsTextArea.onkeyup = function (ev){
+    if(sideBar.getElementsByTagName("textarea").length < 1){
+        emojiTable.innerHTML = "<div id='load'>加载失败，<br>请<a href='https://live.bilibili.com/21652717'>点击这里</a>重试<br><br>" +
+            "如未登录，请先登录</div>";
+        document.getElementById("load").style.marginTop = "130px";
+    }else{
+        commentsTextArea.addEventListener("focus", textareaHandler);
+        commentsTextArea.addEventListener("blur", textareaHandler);
+        function textareaHandler(){
+            commentsTextArea.value = emojiText;
+        }
 
-        console.log(commentsTextArea.value)
-        emojiText = commentsTextArea.value;
-    }
-    // add new emoji here to show in web.
-    emojiTable.innerHTML =
-        "<tbody><tr><td style=\" background:url("+ img0 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img1 +") no-repeat bottom center; background-size: contain\"></td></tr>" +
-        "<tr><td style=\" background:url("+ img2 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img3 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
-        "<tr><td style=\" background:url("+ img4 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img5 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
-        "<tr><td style=\" background:url("+ img6 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img7 +") no-repeat bottom center; background-size: contain\"></td></tr>" +
-        "<tr><td style=\" background:url("+ img8 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img9 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
-        "<tr><td style=\" background:url("+ img10 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img11 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
-        "<tr><td style=\" background:url("+ img12 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img13 +") no-repeat bottom center; background-size: contain\"></td></tr>" +
-        "<tr><td style=\" background:url("+ img14 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img15 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
-        "<tr><td style=\" background:url("+ img16 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
-        "<tr><td colspan=\"2\" style=\" background:url("+ img17 +") no-repeat bottom center; background-size: contain\"></td></tr></tbody>";
+        commentsTextArea.onkeyup = function (ev){
+            emojiText = commentsTextArea.value;
+        }
+        // add new emoji here to show in web.
+        emojiTable.innerHTML =
+            "<tbody><tr><td style=\" background:url("+ img0 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img1 +") no-repeat bottom center; background-size: contain\"></td></tr>" +
+            "<tr><td style=\" background:url("+ img2 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img3 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
+            "<tr><td style=\" background:url("+ img4 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img5 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
+            "<tr><td style=\" background:url("+ img6 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img7 +") no-repeat bottom center; background-size: contain\"></td></tr>" +
+            "<tr><td style=\" background:url("+ img8 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img9 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
+            "<tr><td style=\" background:url("+ img10 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img11 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
+            "<tr><td style=\" background:url("+ img12 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img13 +") no-repeat bottom center; background-size: contain\"></td></tr>" +
+            "<tr><td style=\" background:url("+ img14 +") no-repeat bottom center; background-size: contain\"></td><td style=\" background:url("+ img15 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
+            "<tr><td style=\" background:url("+ img16 +") no-repeat bottom center; background-size: contain\"></td></tr>"+
+            "<tr><td colspan=\"2\" style=\" background:url("+ img17 +") no-repeat bottom center; background-size: contain\"></td></tr></tbody>";
 
-    var emojiText = "";
-    for (let i = 0; i < emojiTable.rows.length; i++) {
-        var cell = emojiTable.rows[i].cells;
-        for (let j = 0; j < cell.length; j++) {
-            cell[j].onclick = function (){
-                addText(emoji[i][j]);
+
+        for (let i = 0; i < emojiTable.rows.length; i++) {
+            var cell = emojiTable.rows[i].cells;
+            for (let j = 0; j < cell.length; j++) {
+                cell[j].onclick = function (){
+                    addText(emoji[i][j]);
+                }
             }
+        }
+
+        function addText(text){
+            emojiText += text;
+            sideBar.getElementsByTagName("textarea")[0].value += text;
         }
     }
 
-    function addText(text){
-        emojiText += text;
-        sideBar.getElementsByTagName("textarea")[0].value += text;
-    }
 }
